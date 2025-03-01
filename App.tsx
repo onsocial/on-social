9274ae7df9132e7eff0c25803cb1a9ea67b8a1d4
greenghostnear <greenghost.ccc@gmail.com>
2025-03-01 15:54:22 +0000

    Set up folder structure

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import '@assets/global.css';
+import HomePage from '@screens/HomePage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <HomePage />
       <StatusBar style="auto" />
     </View>
   );
---
+++ screens/HomePage.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
+import '@assets/global.css';
+
+export default function HomePage() {
+  return (
+    <View style={styles.container}>
+      <Text className="text-blue-400">Under Construction</Text>
+      <TouchableOpacity className="mt-5 p-2 bg-blue-400 rounded">
+        <ActivityIndicator size="small" color="#fff" />
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
