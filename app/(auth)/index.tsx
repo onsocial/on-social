21b347c53fbfb4aa057615a67d19023010cb21e0
greenghostnear <greenghost.ccc@gmail.com>
2025-03-04 15:53:58 +0000

    + Button component

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native';
 import { useRouter } from 'expo-router';
 import ThemeToggle from '@components/ThemeToggle';
 import { Button } from '@components/Button';
+import { StarCIconFilled } from '@assets/icons';
 
 export default function AuthScreen() {
   const router = useRouter();
@@ -9,13 +10,13 @@ export default function AuthScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-light-background dark:bg-dark-background">
       <ThemeToggle />
-
       <Button
         variant="primary"
         size="md"
         className="mt-5"
-        textClassName="font-semibold" // Apply semibold text using Tailwind
+        textClassName="font-semibold"
         onClick={() => router.replace('/(app)')}
+        icon={<StarCIconFilled size={20} />} // Size adjusted for button
       >
         Let's connect
       </Button>
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 // src/@assets/icons/index.ts
 export { SunIcon } from '@assets/icons/general/SunIcon';
 export { MoonIcon } from '@assets/icons/general/MoonIcon';
+export { StarCIconFilled } from '@assets/icons/general/StarCIconFilled';
 // Add more as needed: export { NewIcon } from './NewIcon';
